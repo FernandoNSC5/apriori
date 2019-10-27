afcdbda8fb05474d25995fa2c8286ee8e99551b1
Fernando Nogueira da Silva Costa <fernandonsc5@gmail.com>
2019-10-27 10:41:35 -0300

    Connection alterations - sql inputs

--- connectPg.py
+++ connectPg.py
@@ -4,24 +4,53 @@ class Connect():
 
 	def __init__(self):
 		print("[SYSTEM] Initializing database connection")
+		self.initAllQuerrys()
+
+
+	######################################################
+	##
+	##	Core Methods
+	##
+	######################################################
+	def initConnection(self):
 		try:
 			self.conn = pg.connect(dbname='test', user='postgres', password='oj1s43jhps')
-			print("[SYSTEM] Creating cursor")
 			self.cursor = self.conn.cursor()
-			self.initAllQuerrys()
 		except:
-			print("[SYSTEM] Error trying to connect to database")
+			print("[SYSTEM] An error ocured while trying to connect to the database")
+			self.cursor.close()
+			self.conn.close()
 
 	def initAllQuerrys(self):
 		print("[SQL] Creating querys")
 		self.getAll = "SELECT * FROM test"
 		self.getAllWithoutPk = "SELECT t.coffee, t.bread, t.milk, t.beer FROM test t"
 
+
+	#####################################################
+	##
+	##	SQL Methods
+	##
+	#####################################################
+	def selectAllWithoutPk(self):
+		try:
+			self.initConnection()
+			self.cursor.execute(self.getAllWithoutPk)
+			return self.cursor.fetchall()
+		except:
+			print("[SYSTEM] selectAllWithoutPk - Failure")
+			return -1
+		finally:
+			self.cursor.close()
+			self.conn.close()
+
 	def selectAll(self):
-		self.cursor.execute(self.getAllWithoutPk)
-		self.response = self.cursor.fetchall()
-		print(self.response)
-
-
-C = Connect()
-C.selectAll()
+		try:
+			self.cursor.execut(self.getAll)
+			return self.cursor.fetchall()
+		except:
+			print("[SYSTEM] selectAll - Failure")
+			return -1
+		finally:
+			self.cursor.close()
+			self.conn.close()
